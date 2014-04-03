c21cbdaa436645061469c39476b8c28cb53f4c2e
mickael <mickael.fardilha@gmail.com>
2014-04-03 13:52:45 +0200

    ajout commentaire dans les spec

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -3,36 +3,43 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
+  #Permet de tester que la route par default se dirige bien vers la page d'accueil
   it "devrait trouver une page Accueil a /" do
     get '/'
     response.should have_selector('title', :content => "Accueil")
   end #Fin de it
 
+  #Permet de tester que la route contact se dirige bien vers la page contact
   it "devrait trouver une page Contact at '/contact'" do
     get '/contact'
     response.should have_selector('title', :content => "Contact")
   end #Fin de it
 
+  #Permet de tester que la route about se dirige bien vers la page A propos
   it "should have an a propos page at '/about'" do
     get '/about'
     response.should have_selector('title', :content => "A Propos")
   end #Fin de it
 
+  #Permet de tester que la route help se dirige bien vers la page d'aide
   it "devrait trouver une page aide a help" do
     get '/help'
     response.should have_selector('title', :content => "Aide")
   end #Fin de it
 
+  #Permet de tester que la route signup se dirige bien vers la page d'inscription
   it "devrait avoir une page d'inscription a signup" do
     get '/signup'
     response.should have_selector('title', :content => "Inscription")
   end #Fin de it
 
+  #Permet de tester que la route showUserList se dirige bien vers la liste des utilisateurs
   it "devrait avoir une page qui liste les users a showUserList" do
     get '/showUserList'
     response.should have_selector('title', :content => "Liste des utilisateurs")
   end #Fin de it
 
+  #Permet de tester que la route showNonSportifList se dirige bien vers la liste des utilisateurs non sportifs
   it "devrait avoir une page qui liste les users non sportif a showNonSportifList" do
     get '/showNonSportifList'
     response.should have_selector('title', :content => "Liste des utilisateurs non sportifs")
